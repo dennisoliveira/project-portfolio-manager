201dd8930aecbd3ee5c44c27227912957fd645da
Dennis de Oliveira <contato@dennisoliveira.com.br>
2025-08-18 10:47:17 -0300

    first commit

---
+++ src/main/java/com/github/dennisoliveira/portfolio/ProjectPortfolioManagerApplication.java
@@ -0,0 +1,13 @@
+package com.github.dennisoliveira.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPortfolioManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectPortfolioManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/dennisoliveira/portfolio/ProjectPortfolioManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dennisoliveira.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPortfolioManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
